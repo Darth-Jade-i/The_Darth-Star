b9d5b50ce3dacaf53063c3292b08ef810bb38eca
Solomon "Darth Jade-i" Okomowho <kkfsolutions.ng@gmail.com>
2024-10-19 23:25:59 +0100

    Edited server.h

--- xerver/c_server/include/server.h
+++ xerver/c_server/include/server.h
@@ -1,33 +1,11 @@
 #ifndef XERVER_H
 #define XERVER_H
 
-/* C Header files */
-#include <arpa/inet.h>
-#include <ctype.h>
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <pthread.h>
-#include <regex.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <stddef.h>
 
-/* Xerver Header Files */
-#include "xerver_structs.h"
-#include "http_utils.h"
-#include "file_utils.h"
-#include "network_utils.h"
-#include "handle_client.h"
-
-/*MACROS*/
 #define PORT 8080
-#define BUFFER_SIZE 104857600
+#define MAX_CONNECTIONS 10
+
+void run_server(void);
 
 #endif /* XERVER_H */
