1e013e3a22c52d4cc332e34351637e916c9b4f67
Solomon "Darth Jade-i" Okomowho <kkfsolutions.ng@gmail.com>
2024-10-19 19:33:02 +0100

    Edited file_utils.h header file
    - Added comments
    - Optimized to conform with modularity

--- xerver/c_server/include/file_utils.h
+++ xerver/c_server/include/file_utils.h
@@ -1,7 +1,7 @@
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
-bool case_insensitive_compare(const char *s1, const char *s2);
 char *get_file_case_insensitive(const char *file_name);
+char *url_decode(const char *src);
 
-#endif
+#endif // FILE_UTILS_H
