2ca497535bd5f350b794073bf9acb9459214a749
Solomon "Darth Jade-i" Okomowho <kkfsolutions.ng@gmail.com>
2024-10-19 18:01:57 +0100

    Created the files_utils.h header file
    This contains the protoypes that will concer
    - file searching
    - file handling

--- xerver/c_server/http_utils.c
+++
@@ -1,3 +0,0 @@
-const char *get_file_extension(const char *file_name);
-const char *get_mime_type(const char *file_ext);
-void build_http_response(...);
---
+++ xerver/c_server/include/file_utils.h
@@ -0,0 +1,7 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+bool case_insensitive_compare(const char *s1, const char *s2);
+char *get_file_case_insensitive(const char *file_name);
+
+#endif
