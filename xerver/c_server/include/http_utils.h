7d0ff9a7cc758fcbc0b95eca068675bde4b156b4
Solomon "Darth Jade-i" Okomowho <kkfsolutions.ng@gmail.com>
2024-10-19 19:29:11 +0100

    Edited http_utils.h header file
    - Adjusted for modularity
    - Added some commenets

--- xerver/c_server/include/http_utils.h
+++ xerver/c_server/include/http_utils.h
@@ -6,4 +6,4 @@ const char *get_mime_type(const char *file_ext);
 char *url_decode(const char *src);
 void build_http_response(const char *file_name, const char *file_ext, char *response, size_t *response_len);
 
-#endif
+#endif /* HTTP_UTILS_H */
