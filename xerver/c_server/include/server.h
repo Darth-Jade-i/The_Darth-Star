1c5263b705322af02d807150f19bae6e693a6426
Solomon "Darth Jade-i" Okomowho <kkfsolutions.ng@gmail.com>
2024-10-19 19:52:47 +0100

    Edited server.h file

--- xerver/c_server/include/server.h
+++ xerver/c_server/include/server.h
@@ -1,6 +1,7 @@
 #ifndef XERVER_H
 #define XERVER_H
 
+/* C Header files */
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <dirent.h>
@@ -18,12 +19,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/* Xerver Header Files */
+#include "xerver_structs.h"
 #include "http_utils.h"
 #include "file_utils.h"
 #include "network_utils.h"
 #include "handle_client.h"
 
+/*MACROS*/
 #define PORT 8080
 #define BUFFER_SIZE 104857600
 
-#endif
+#endif /* XERVER_H */
